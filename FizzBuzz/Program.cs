4d7d87b1c918454beb77e64337b602d46a0b30d6
sheronan <snaidoo@bwlog.com>
2014-02-18 09:46:06 +0200

    Added Fizz Buzz solution
    Prework 1

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                result = i.ToString();
+                if (i % 15 == 0)
+                    result = "FizzBuzz";
+                else if (i % 5 == 0)
+                    result = "Buzz";
+                else if (i % 3 == 0)
+                    result = "Fizz";
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
